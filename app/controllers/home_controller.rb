0bdfadc8472bec76c4605c1cdddfc875660a5e77
Louis Hoang <bawsing@gmail.com>
2015-11-07 11:37:45 -0800

    added model post and set relationship

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,7 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    @post = Post.new
+    @all_posts = Post.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
+
+  resources :posts
 end
