573c87a0a61df44a0b1521d442c0ceb9135b4c8f
Louis Hoang <bawsing@gmail.com>
2015-11-08 20:01:24 -0800

    fixed mandrill key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
     :port =>           '587',
     :address =>        'smtp.mandrillapp.com',
     :user_name =>      ENV['MANDRILL_USERNAME'],
-    :password =>       ENV['MANDRILL_KEY'],
+    :password =>       ENV['MANDRILL_APIKEY'],
     :domain =>         'heroku.com',
     :authentication => :plain
   }
